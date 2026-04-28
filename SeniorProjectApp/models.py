a65ade030b44920b0b53d51385035c9ce41ede53
alashaath <allashaath@yahoo.com>
2026-04-28 15:28:58 -0500

    use cloudinary fields

--- SeniorProjectApp/models.py
+++ SeniorProjectApp/models.py
@@ -1,42 +1,34 @@
 from django.db import models
 from django.conf import settings
-from django import forms
 from django.contrib.auth.models import User
-import datetime
+from cloudinary.models import CloudinaryField
 
 User = settings.AUTH_USER_MODEL
 
-# Create your models here.
-
 class MenuItem(models.Model):
-   name = models.CharField(max_length=100)
-   price = models.DecimalField(max_digits=10, decimal_places=2)
-   image = models.ImageField(upload_to='uploads/menuitems/', default='static/default_image.png')
-   description = models.TextField(max_length=500, blank=True)  # New field for description
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = CloudinaryField('image', default='static/default_image.png')
+    description = models.TextField(max_length=500, blank=True)
+    def __str__(self):
+        return self.name
 
-
-   def __str__(self):
-       return self.name
-   
 class MenuItemCategory(models.Model):
     name = models.CharField(max_length=100)
     menu_items = models.ManyToManyField(MenuItem)
-
     def __str__(self):
         return self.name
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     categories = models.ManyToManyField(MenuItemCategory)
-
     def __str__(self):
         return self.name
 
 class Carausel(models.Model):
-    image = models.ImageField(upload_to= 'uploads/carausel/')
+    image = CloudinaryField('image')
     title = models.CharField(max_length=150)
     sub_title = models.CharField(max_length=100)
-
     def __str__(self):
         return str(self.id)
 
